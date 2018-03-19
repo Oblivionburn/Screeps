558239ff30f78d33cd1ca413ac50bfd30ecc95ab
Oblivionburn <donney.lane@gmail.com>
2018-03-19 06:40:08 -0500

    v1.1

--- handle.Spawning.js
+++ handle.Spawning.js
@@ -57,13 +57,6 @@ function HandleSpawning()
         if(spawn.spawning) 
         {
             var spawningCreep = Game.creeps[spawn.spawning.name];
-            spawn.room.visual.text
-            (
-                '🛠️' + spawningCreep.memory.role,
-                spawn.pos.x + 1,
-                spawn.pos.y,
-                {align: 'left', opacity: 0.8}
-            );
         }
     }
 }
