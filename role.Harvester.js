e4d54b9c67fe5c0d151b6da57765318b8d6427ca
Oblivionburn <donney.lane@gmail.com>
2018-03-20 04:28:40 -0500

    Update role.Harvester.js

--- role.Harvester.js
+++ role.Harvester.js
@@ -19,56 +19,6 @@ function Harvester(creep)
         }
     }
     
-    if (okay &&
-        creep.carry.energy > 0)
-    {
-        var spawn = GetStructure(creep, "Spawn", true);
-        if (spawn != null) 
-        {
-            okay = false;
-            Transfer(creep, spawn);
-        }
-    }
-    
-    if (okay &&
-        creep.carry.energy > 0)
-    {
-        var extension = GetStructure(creep, "Extension", true);
-        if (extension != null)
-        {
-            okay = false;
-            Transfer(creep, extension);
-        }
-    }
-    
-    if (okay &&
-        creep.carry.energy > 0)
-    {
-        var tower = GetStructure(creep, "Tower", true);
-        if (tower != null) 
-        {
-            okay = false;
-            Transfer(creep, tower);
-        }
-    }
-    
-    if (okay &&
-        creep.carry.energy > 0)
-    {
-        for (var name in Game.creeps) 
-        {
-            var builder = Game.creeps[name];
-            if (builder.memory.role == 'Builder' &&
-                builder.carry.energy < builder.carryCapacity &&
-                Available(creep, builder.id)) 
-            {
-                okay = false;
-                Transfer(creep, builder);
-                break;
-            }
-        }
-    }
-    
     if (okay)
     {
         creep.memory.task = "Harvesting";
