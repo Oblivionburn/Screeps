317b7c7527482e4f2c10d43f7fc0ec919f08e8b0
Oblivionburn <donney.lane@gmail.com>
2023-12-17 09:20:19 -0600

    v5.3

--- util.GetSiphonTarget.js
+++ util.GetSiphonTarget.js
@@ -1,10 +1,18 @@
 const Position = require("object.Position");
 const Targeted = require("util.Targeted");
 const GetNearest = require("util.GetNearest");
+const GetGrave = require("util.GetGrave");
 
 function GetSiphonTarget(creep)
 {
-    const extensions = [];
+    const grave = GetGrave(creep);
+    if (grave != null)
+    {
+        return grave;
+    }
+    
+    const sites = [];
+    let sitesCount = 0;
     
     const allStructures = creep.room.find(FIND_MY_STRUCTURES);
     const structureCount = allStructures.length;
@@ -16,37 +24,53 @@ function GetSiphonTarget(creep)
         if (structure.structureType == STRUCTURE_EXTENSION &&
             structure.store[RESOURCE_ENERGY] > 0)
         {
-            extensions.push(structure);
+            sites.push(structure);
         }
     }
+    sitesCount = sites.length;
     
-    const extensionCount = extensions.length;
+    if (sitesCount == 0)
+    {
+        const ruins = creep.room.find(FIND_RUINS);
+        const ruinsCount = ruins.length;
+        
+        for (let i = 0; i < ruinsCount; i++)
+        {
+            const ruin = ruins[i];
+            
+            if (ruin.store[RESOURCE_ENERGY] > 0)
+            {
+                sites.push(ruin);
+            }
+        }
+    }
+    sitesCount = sites.length;
     
     const positions = [];
-    for (let i = 0; i < extensionCount; i++)
+    for (let i = 0; i < sitesCount; i++)
     {
-        const extension = extensions[i];
+        const site = sites[i];
         
-        const position = new Position(extension.pos.x, extension.pos.y);
+        const position = new Position(site.pos.x, site.pos.y);
         
-        if (!Targeted(creep, extension.id))
+        if (!Targeted(creep, site.id))
         {
             positions.push(position);
         }
     }
-    
-    if (positions.length > 0)
+    const positionsCount = positions.length;
+    if (positionsCount > 0)
     {
         const nearest = GetNearest(creep.pos.x, creep.pos.y, positions);
         
-        for (let i = 0; i < extensionCount; i++)
+        for (let i = 0; i < sitesCount; i++)
         {
-            const extension = extensions[i];
+            const site = sites[i];
             
-            if (extension.pos.x == nearest.X &&
-                extension.pos.y == nearest.Y)
+            if (site.pos.x == nearest.X &&
+                site.pos.y == nearest.Y)
             {
-                return extension;
+                return site;
             }
         }
     }
