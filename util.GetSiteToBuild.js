ceb461ccb8416229a4bdb420e7154daad1ec6569
Oblivionburn <donney.lane@gmail.com>
2023-12-18 06:36:28 -0600

    v5.6

--- util.GetSiteToBuild.js
+++ util.GetSiteToBuild.js
@@ -4,65 +4,71 @@ const GetNearest = require("util.GetNearest");
 
 function GetSiteToBuild(creep)
 {
+    let chosen = null;
+    
     const sites = creep.room.find(FIND_MY_CONSTRUCTION_SITES);
     const sitesCount = sites.length;
 
-    let chosen = sites[0];
-    
-    //Get biggest site to start with
-    for (let i = 0; i < sitesCount; i++)
+    if (sitesCount > 0)
     {
-        const site = sites[i];
-        
-        if (site.progressTotal > chosen.progressTotal)
-        {
-            chosen = site;
-        }
-    }
+        let chosen = sites[0];
     
-    //Check for similar site that is closer to being built
-    for (let i = 0; i < sitesCount; i++)
-    {
-        const site = sites[i];
-        
-        if (site.progress > chosen.progress &&
-            site.progressTotal == chosen.progressTotal)
-        {
-            chosen = site;
-        }
-    }
-    
-    //If no similar sites started yet, get nearest
-    if (chosen.progress == 0)
-    {
-        const positions = [];
-        
-        //Get sites not targeted by other creeps
+        //Get biggest site to start with
         for (let i = 0; i < sitesCount; i++)
         {
             const site = sites[i];
             
-            if (!Targeted(creep, site.id) &&
-                site.progressTotal == chosen.progressTotal)
+            if (site.progressTotal > chosen.progressTotal)
             {
-                positions.push(new Position(site.pos.x, site.pos.y));
+                chosen = site;
             }
         }
         
-        const positionCount = positions.length;
-        if (positionCount > 0)
+        //Check for similar site that is closer to being built
+        for (let i = 0; i < sitesCount; i++)
         {
-            const nearest = GetNearest(creep.pos.x, creep.pos.y, positions);
+            const site = sites[i];
             
-            //Get site with nearest position
+            if (site.progress > chosen.progress &&
+                site.progressTotal == chosen.progressTotal)
+            {
+                chosen = site;
+            }
+        }
+        
+        //If no similar sites started yet, get nearest
+        if (chosen != null &&
+            chosen.progress == 0)
+        {
+            const positions = [];
+            
+            //Get sites not targeted by other creeps
             for (let i = 0; i < sitesCount; i++)
             {
                 const site = sites[i];
                 
-                if (site.pos.x == nearest.X &&
-                    site.pos.y == nearest.Y)
+                if (!Targeted(creep, site.id) &&
+                    site.progressTotal == chosen.progressTotal)
                 {
-                    return site;
+                    positions.push(new Position(site.pos.x, site.pos.y));
+                }
+            }
+            
+            const positionCount = positions.length;
+            if (positionCount > 0)
+            {
+                const nearest = GetNearest(creep.pos.x, creep.pos.y, positions);
+                
+                //Get site with nearest position
+                for (let i = 0; i < sitesCount; i++)
+                {
+                    const site = sites[i];
+                    
+                    if (site.pos.x == nearest.X &&
+                        site.pos.y == nearest.Y)
+                    {
+                        return site;
+                    }
                 }
             }
         }
